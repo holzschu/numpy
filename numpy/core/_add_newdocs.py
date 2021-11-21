50d630cf8010d4d53c758be6b8fe8302cb911a33
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2021-11-21 20:31:32 +0100

    ????

--- numpy/core/_add_newdocs.py
+++ numpy/core/_add_newdocs.py
@@ -4778,40 +4778,36 @@ add_newdoc('numpy.core.multiarray', '_set_madvise_hugepage',
     See `global_state` for more information.
     """)
 
-# iOS: disable _multiarray_tests to save (a bit) on disk space
-import sys
-import os
-if not(sys.platform == 'darwin' and os.uname().machine.startswith('iP')): 
-    add_newdoc('numpy.core._multiarray_tests', 'format_float_OSprintf_g',
-        """
-        format_float_OSprintf_g(val, precision)
-    
-        Print a floating point scalar using the system's printf function,
-        equivalent to:
-    
-            printf("%.*g", precision, val);
-    
-        for half/float/double, or replacing 'g' by 'Lg' for longdouble. This
-        method is designed to help cross-validate the format_float_* methods.
-    
-        Parameters
-        ----------
-        val : python float or numpy floating scalar
-            Value to format.
-    
-        precision : non-negative integer, optional
-            Precision given to printf.
-    
-        Returns
-        -------
-        rep : string
-            The string representation of the floating point value
-    
-        See Also
-        --------
-        format_float_scientific
-        format_float_positional
-        """)
+add_newdoc('numpy.core._multiarray_tests', 'format_float_OSprintf_g',
+    """
+    format_float_OSprintf_g(val, precision)
+
+    Print a floating point scalar using the system's printf function,
+    equivalent to:
+
+        printf("%.*g", precision, val);
+
+    for half/float/double, or replacing 'g' by 'Lg' for longdouble. This
+    method is designed to help cross-validate the format_float_* methods.
+
+    Parameters
+    ----------
+    val : python float or numpy floating scalar
+        Value to format.
+
+    precision : non-negative integer, optional
+        Precision given to printf.
+
+    Returns
+    -------
+    rep : string
+        The string representation of the floating point value
+
+    See Also
+    --------
+    format_float_scientific
+    format_float_positional
+    """)
 
 
 ##############################################################################
